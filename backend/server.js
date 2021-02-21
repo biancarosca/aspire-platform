97e5bab59af56b55d01c2828d6e7d5c8d2dfe425
Bianca <rosca.bianca@yahoo.com>
2021-02-21 23:59:39 +0200

    added images, eslint config

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 
 const app = express();
 
@@ -7,8 +7,8 @@ app.use(cors());
 
 const PORT = process.env.PORT || 5000;
 
-app.get('/', (req,res) => {
-    res.send('Server is running');
-})
+app.get("/", (req, res) => {
+	res.send("Server is running");
+});
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
