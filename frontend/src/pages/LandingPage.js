382c10838d2922e2933e3cc7e2e550b8b927465a
Bianca <rosca.bianca@yahoo.com>
2021-02-22 14:28:23 +0200

    landing page mobile responsiveness

--- frontend/src/pages/LandingPage.js
+++ frontend/src/pages/LandingPage.js
@@ -3,6 +3,8 @@ import logo from "../images/logo-min.png";
 import hero from "../images/hero.jpg";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 const LandingPage = () => {
 	return (
@@ -25,6 +27,7 @@ const LandingPage = () => {
 						</StyledLink>
 					</li>
 				</ul>
+				<StyIcon icon={faBars} ></StyIcon>
 			</StyledNav>
 			<StyledHero>
 				<div className="info">
@@ -44,19 +47,39 @@ const LandingPage = () => {
 const StyWrapper = styled.div`
 	.cta-btn {
 		transition: all 0.2s ease;
-		transform-origin:left center;
+		transform-origin: left center;
 		&:hover {
 			transform: scale(1.04);
 			color: white;
 		}
+	}
+	margin-left: auto;
+	margin-right: auto;
+	width: 960px;
+	@media (max-width: 1030px) {
+		width: 800px;
+	}
+	@media (max-width: 870px) {
+		width: 700px;
+	}
+	@media (max-width: 710px) {
+		width: 600px;
+	}
+	@media (max-width: 610px) {
+		width: 450px;
+	}
+	@media (max-width: 500px) {
+		width: 350px;
+	}
+	@media (max-width: 380px) {
+		width: 260px;
 	}
 `;
 
 const StyledNav = styled.nav`
 	display: flex;
 	min-height: 7vh;
-	width: 80%;
-	margin: 3% 10%;
+	margin: 3% 0%;
 	align-items: center;
 	justify-content: space-between;
 	ul {
@@ -84,13 +107,25 @@ const StyledNav = styled.nav`
 			color: white;
 		}
 	}
+	.hamburger {
+		width: 30px;
+		height: auto;
+	}
+	@media (max-width: 870px) {
+		justify-content: space-around;
+	}
+	@media (max-width: 610px) {
+		justify-content: space-between;
+		ul {
+			display: none;
+		}
+	}
 `;
 
 const StyledHero = styled.div`
 	display: flex;
 	align-items: center;
-	width: 80%;
-	margin: 0 10%;
+	justify-content: space-between;
 	.info {
 		h1 {
 			margin-bottom: 1.5rem;
@@ -104,6 +139,57 @@ const StyledHero = styled.div`
 		padding: 0.5rem 3rem;
 		border-radius: 5rem;
 	}
+	img {
+		z-index: -1;
+	}
+	@media (max-width: 870px) {
+		flex-direction: column;
+		.info {
+			h1 {
+				text-align: center;
+			}
+		}
+		.cta-btn {
+			display: flex;
+			justify-content: center;
+			transform-origin: center;
+		}
+		.get-started {
+			margin-top: 0;
+			margin-bottom: 1.5rem;
+		}
+	}
+	@media (max-width: 710px) {
+		.info {
+			h1 {
+				font-size: 1.8rem;
+			}
+		}
+	}
+	@media (max-width: 610px) {
+		img {
+			width: 400px;
+			height: 400px;
+		}
+	}
+	@media (max-width: 500px) {
+		img {
+			width: 250px;
+			height: 250px;
+		}
+		.info{
+			h1{
+				font-size: 1.5rem;
+			}
+		}
+	}
+	@media (max-width:380px){
+		.info{
+			h1{
+				font-size: 1.2rem;
+			}
+		}
+	}
 `;
 
 const StyledLink = styled(Link)`
@@ -111,6 +197,14 @@ const StyledLink = styled(Link)`
 	color: #434246;
 	&:hover {
 		color: black;
+	}
+`;
+
+const StyIcon = styled(FontAwesomeIcon)`
+	display: none;
+	cursor: pointer;
+	@media (max-width: 610px){
+		display: inline;
 	}
 `;
 
