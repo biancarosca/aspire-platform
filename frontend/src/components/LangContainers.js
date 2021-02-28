054e8329e48ce91a8656648ec05ffcd13476db27
Bianca <rosca.bianca@yahoo.com>
2021-02-28 21:07:35 +0200

    language input

--- frontend/src/components/LangContainers.js
+++ frontend/src/components/LangContainers.js
@@ -1,34 +1,53 @@
 import React from "react";
 //packages
+import PropTypes from "prop-types";
 import styled from "styled-components";
+//assets
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTimes } from "@fortawesome/free-solid-svg-icons";
+//components
+import {
+	StyWrapper,
+} from "../components/GlobalStyles";
 
-const LangContainers = () => {
+
+const LangContainers = ({ languages }) => {
+	const handleDeleteLang = (lang) => {
+		console.log(lang);
+	};
+
 	return (
 		<StyLangWrap>
-			<span className="lang-wrap">
-				<span className="lang">French</span>
-				<StyClose icon={faTimes} />
-			</span>
-			<span className="lang-wrap">
-				<span className="lang">Spanish</span>
-				<StyClose icon={faTimes} />
-			</span>
+			{languages.map((lang) => (
+				<span className="lang-wrap" key={lang}>
+					<span className="lang">{lang}</span>
+					<StyClose
+						id={lang}
+						icon={faTimes}
+						onClick={() => handleDeleteLang(lang)}
+					/>
+				</span>
+			))}
 		</StyLangWrap>
 	);
+};
+
+LangContainers.propTypes = {
+	languages: PropTypes.array,
 };
 
 const StyClose = styled(FontAwesomeIcon)`
 	cursor: pointer;
 	color: #8c8e91;
-    margin-right: 0.3rem;
+	margin-right: 0.3rem;
 `;
 
-const StyLangWrap = styled.div`
+const StyLangWrap = styled(StyWrapper)`
 	display: flex;
+	flex-wrap: wrap;
 	.lang-wrap {
 		background-color: #d4d5d6;
+		margin-top: 1rem;
 		margin-right: 0.5rem;
 		display: flex;
 		align-items: center;
--- frontend/src/pages/CreateProfile.js
+++ frontend/src/pages/CreateProfile.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import { useSelector } from "react-redux";
 //packages
 import styled from "styled-components";
@@ -15,6 +15,17 @@ import LangContainers from "../components/LangContainers";
 
 const CreateProfile = () => {
 	const role = useSelector((store) => store.pickedRole);
+	const [languages, setLanguages] = useState([]);
+
+	const handleLanguagesInput = (target) => {
+		if (target.value.indexOf(",") !== -1) {
+			const lang = target.value;
+			const langSanitized = lang.charAt(0).toUpperCase() + lang.toLowerCase().slice(1,lang.length-1); //capitalize
+			setLanguages([...languages, langSanitized]);
+			target.value = "";
+		}
+	};
+
 	return (
 		<StyWrapper>
 			<LandingNav />
@@ -38,13 +49,17 @@ const CreateProfile = () => {
 							// value={lastName}
 						/>
 						<StyInput
-							// onChange={({ target }) => setEmail(target.value)}
+							onChange={({ target }) =>
+								handleLanguagesInput(target)
+							}
 							type="text"
 							placeholder="Languages"
 							size="25"
 							// value={email}
 						/>
-						<LangContainers />
+						<LangContainers
+							languages={languages}
+						/>
 						<StyBtn className="cta-btn" type="submit">
 							Continue
 						</StyBtn>
