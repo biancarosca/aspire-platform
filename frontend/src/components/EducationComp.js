33af31eabe0524e84763576618f8f6f7d01df703
Bianca <rosca.bianca@yahoo.com>
2021-03-01 23:29:31 +0200

    education field , add, remove

--- frontend/src/components/EducationComp.js
+++ frontend/src/components/EducationComp.js
@@ -1,29 +1,54 @@
 import React from "react";
 //packages
 import styled from "styled-components";
+import PropTypes from "prop-types";
 //components
-import { StyBtn } from "../components/GlobalStyles";
+import { StyInput, StyBtn } from "../components/GlobalStyles";
 
-const EducationComp = () => {
+const EducationComp = ({ id, setNumEducField }) => {
+	const handleRemove = (e) => {
+		e.preventDefault();
+		setNumEducField((prev) => prev.filter((currId) => currId !== id));
+	};
+
 	return (
 		<StyWrapper>
-			<span className="education-title">Education</span>
-			<StyAddEduc>+</StyAddEduc>
+			<StyRemove onClick={(e) => handleRemove(e)}>-</StyRemove>
+			<StyInput type="text" placeholder="Institution" size="25" />
+			<StyInput type="text" placeholder="Degree/title" size="25" />
+			<StyInput type="date" />
+			<StyInput type="date" />
 		</StyWrapper>
 	);
 };
 
-const StyWrapper = styled.div`
-	.education-title {
-		color: #434246;
-        margin-right: 1rem;
-	}
-`;
+EducationComp.propTypes = {
+	id: PropTypes.string,
+	setNumEducField: PropTypes.func,
+};
 
-const StyAddEduc = styled(StyBtn)`
+const StyRemove = styled(StyBtn)`
 	width: 30px;
 	height: 30px;
 	padding: 0;
+	margin: 0;
+	align-self: flex-end;
+`;
+
+const StyWrapper = styled.div`
+	display: flex;
+	flex-direction: column;
+	.education-title {
+		color: #434246;
+		margin-right: 1rem;
+	}
+	input {
+		width: 100%;
+	}
+	input[type="date"] {
+		color: #434246;
+		width: 60%;
+	}
 `;
 
 export default EducationComp;
--- frontend/src/pages/CreateProfile.js
+++ frontend/src/pages/CreateProfile.js
@@ -3,6 +3,7 @@ import { useSelector } from "react-redux";
 //packages
 import styled from "styled-components";
 import { toast } from "react-toastify";
+import uuid from "react-uuid";
 //components
 import LandingNav from "../components/LandingNav";
 import {
@@ -20,6 +21,7 @@ import { languagesData } from "../utils/languagesData";
 const CreateProfile = () => {
 	const role = useSelector((store) => store.pickedRole);
 	const [languages, setLanguages] = useState([]);
+	const [numEducField, setNumEducField] = useState([]);		//keep track of education input groups 
 
 	const handleLanguagesInput = (target) => {
 		if (target.value.indexOf(",") !== -1) {
@@ -39,6 +41,11 @@ const CreateProfile = () => {
 			else toast.error("Not a valid language!");
 			target.value = "";
 		}
+	};
+
+	const handleAddEduc = (e) => {
+		e.preventDefault();
+		setNumEducField((prev) => [...prev, uuid()]);		//assign to each education input group a unique id
 	};
 
 	return (
@@ -76,7 +83,24 @@ const CreateProfile = () => {
 							languages={languages}
 							setLanguages={setLanguages}
 						/>
-						<EducationComp />
+						<div className="education-wrapper">
+							<div className="education-header">
+								<span className="education-title">
+									Education
+								</span>
+								<StyAddEduc onClick={(e) => handleAddEduc(e)}>
+									+
+								</StyAddEduc>
+							</div>
+							{numEducField &&
+								numEducField.map((id) => (
+									<EducationComp
+										key={id}
+										id={id}
+										setNumEducField={setNumEducField}
+									/>
+								))}
+						</div>
 						<StyBtn className="cta-btn" type="submit">
 							Continue
 						</StyBtn>
@@ -87,6 +111,13 @@ const CreateProfile = () => {
 	);
 };
 
+const StyAddEduc = styled(StyBtn)`
+	width: 30px;
+	height: 30px;
+	padding: 0;
+	margin: 0;
+`;
+
 const StylContainer = styled(StyContainer)`
 	margin-bottom: 5rem;
 	form {
@@ -94,6 +125,19 @@ const StylContainer = styled(StyContainer)`
 	}
 	.cta-btn {
 		transform-origin: center;
+	}
+	.education-wrapper {
+		display: flex;
+		flex-direction: column;
+		margin-top: 1.5rem;
+	}
+	.education-header {
+		display: flex;
+		width: 100%;
+		align-items: center;
+	}
+	.education-title {
+		padding: 0 0.5rem;
 	}
 `;
 
