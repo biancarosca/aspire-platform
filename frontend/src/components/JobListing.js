b75b674687eaf24664c48fb2953b9c6f727bab28
Bianca <rosca.bianca@yahoo.com>
2021-03-29 20:23:36 +0300

    job listing component styled

---
+++ frontend/src/components/JobListing.js
@@ -0,0 +1,121 @@
+import React from "react";
+//components
+import { StyBtn } from "../components/GlobalStyles";
+//packages
+import styled from "styled-components";
+//assets
+import portrait from "../images/portrait.png";
+
+const JobListing = () => {
+	return (
+		<StyJobContainer>
+			<div className="img-container">
+				<img src={portrait} alt="portrait" />
+			</div>
+			<div className="job-details">
+				<div className="job-header">
+					<h3>Junior frontend developer</h3>
+					<div className="technologies-container">
+						<span>Javascript</span>
+						<span>React</span>
+					</div>
+				</div>
+				<div className="job-info-container">
+					<div className="job-info-details">
+						<p className="company-name">IT Solutions</p>
+						<p>
+							<span>Job level:</span> Junior
+						</p>
+						<p>
+							<span>Min. experience:</span> 0 years{" "}
+						</p>
+						<p>
+							<span>Location:</span> Europe (Remote)
+						</p>
+						<p>
+							<span>Salary range:</span> 30k-50k $
+						</p>
+						<p>
+							<span>Employment type:</span> Full-time
+						</p>
+					</div>
+					<div className="btn-container">
+						<StySeeMore>See More</StySeeMore>
+					</div>
+				</div>
+			</div>
+		</StyJobContainer>
+	);
+};
+
+const StySeeMore = styled(StyBtn)`
+	margin: 0;
+	@media (max-width: 790px) {
+		margin-right: 1rem;
+	}
+`;
+
+const StyJobContainer = styled.div`
+	display: flex;
+	margin: 1rem;
+	border: 2px solid #413feb;
+	padding: 1rem;
+	@media (max-width: 790px) {
+		display: block;
+	}
+	p {
+		span {
+			font-weight: bold;
+		}
+	}
+	.job-info-container {
+		display: flex;
+		justify-content: space-between;
+		@media (max-width: 790px) {
+			display: block;
+		}
+	}
+	.btn-container {
+		display: flex;
+		align-items: flex-end;
+		flex-grow: 1;
+		justify-content: flex-end;
+		@media (max-width: 790px) {
+			justify-content: center;
+			margin-top: 1rem;
+		}
+	}
+	.img-container {
+		display: flex;
+		align-items: flex-start;
+	}
+	img {
+		width: 100px;
+		height: 100px;
+	}
+	.job-details {
+		margin-left: 1rem;
+		width: 100%;
+		.company-name {
+			font-style: italic;
+			margin-bottom: 1rem;
+		}
+		.technologies-container {
+			span {
+				margin-right: 1rem;
+				background-color: #eb953f;
+				color: white;
+				border-radius: 5rem;
+				padding: 0 0.3rem;
+				font-size: 0.8rem;
+			}
+		}
+	}
+	@media (max-width: 790px) {
+		.job-details {
+			margin: 0;
+		}
+	}
+`;
+
+export default JobListing;
--- frontend/src/pages/JobsPage.js
+++ frontend/src/pages/JobsPage.js
@@ -1,20 +1,28 @@
-import React from "react";
+import React, {useState} from "react";
 //packages
 import styled from "styled-components";
 //components
 import { StyWrapper } from "../components/GlobalStyles";
 import LandingNav from "../components/LandingNav";
 import LogoutBtn from "../components/LogoutBtn";
+import JobListing from "../components/JobListing";
 //assets
 import defaultPortrait from "../images/portrait.png";
 //redux
 import { useSelector } from "react-redux";
 
-
 const JobsPage = () => {
+	const [selectedCategory, setSelectedCategory] = useState("Jobs");
 	const user = JSON.parse(localStorage.getItem("user"));
-	const role = useSelector(store => store.pickedRole);
-	const avatar = role === "developer" ?  (user.dev.profile.avatar ? user.dev.profile.avatar : "") : (user.recruiter.profile.avatar ? user.recruiter.profile.avatar : "") ;
+	const role = useSelector((store) => store.pickedRole);
+	const avatar =
+		role === "developer"
+			? user.dev.profile.avatar
+				? user.dev.profile.avatar
+				: ""
+			: user.recruiter.profile.avatar
+				? user.recruiter.profile.avatar
+				: "";
 	return (
 		<StyWrapper>
 			<LandingNav
@@ -27,14 +35,28 @@ const JobsPage = () => {
 				<LogoutBtn />
 				<StyAvatar src={avatar ? avatar : defaultPortrait} />
 			</LandingNav>
-			<ul>
-				<li>Jobs</li>
-				<li>P2P</li>
-			</ul>
+			<StyListOptions>
+				<li style={selectedCategory === "Jobs" ? {borderBottom: "2px solid #4347ea"} : {}} onClick={() => setSelectedCategory("Jobs")}>Jobs</li>
+				<li style={selectedCategory === "P2P" ? {borderBottom: "2px solid #4347ea"} : {}} onClick={() => setSelectedCategory("P2P")}>P2P</li>
+			</StyListOptions>
+			<JobListing />
 		</StyWrapper>
 	);
 };
 
+const StyListOptions = styled.ul`
+	list-style-type: none;
+	display: flex;
+	margin-left: 1rem;
+	li{
+		margin-right: 2rem;
+		font-size: 1.1rem;
+		cursor: pointer;
+		&:hover{
+			color: black;
+		}
+	}
+`;
 
 const StyAvatar = styled.img`
 	width: 30px;
