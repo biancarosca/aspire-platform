7b1bed43095a0e31627d8a97ff300766e67d2d6d
Bianca <rosca.bianca@yahoo.com>
2021-03-29 23:27:41 +0300

    backend cors origin

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const authRouter = require("./routers/authRouter");
 const app = express();
 
 //middlewares
-app.use(cors({ origin: "*" }));
+app.use(cors({ origin: "http://localhost:3000", credentials: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use("/api", devRouter);
